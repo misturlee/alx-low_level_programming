d74196fe04ae7f6fe88d424c07b63d6db4d9a1ef
misturlee <vugerstar@gmail.com>
2022-03-10 09:11:49 -0800

    lets try again

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
-
 #include <stdio.h>
-int main (void)
+/**
+ * main - Should always exist in a c program 
+ *
+ * return - 0 means success
+ */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
