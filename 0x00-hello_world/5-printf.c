2474f2e48fe9b3b9c103ef38c3bcbec342481920
misturlee <vugerstar@gmail.com>
2022-03-10 09:23:14 -0800

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Contains main section of program
+ *
+ * Return - 0 means success
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+	}
