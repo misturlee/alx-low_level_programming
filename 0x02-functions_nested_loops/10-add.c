b3f1249b856bb5d1ff462c0506924debdd6725f0
misturlee <vugerstar@gmail.com>
2022-03-15 14:03:37 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - to compute addition
+ * @x: integer to add to y
+ * @y: integer to add to x
+ * Return: Void.
+ */
+
+int add(int x, int y)
+{
+	return (x + y);
+}
+
