ca496d3fb9d5855c55fd2748cb62579e9fb59760
misturlee <vugerstar@gmail.com>
2022-03-15 12:55:04 -0700

    retry

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,8 @@
 #include <unistd.h>
+#include <stdio.h>
 
 /**
- * main - writes the character c to stdout
- * @c: The character to print
+ * main - writes the putchar to the screen
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
