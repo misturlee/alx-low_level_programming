2a8bb0046418612999610e65021707a7e23e5f6c
misturlee <vugerstar@gmail.com>
2022-03-15 13:41:22 -0700

    getting the hang

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign - print the sign of numbers
+ * @n: the number that is being tested
+ *
+ * Return: 0 is sucess and 1
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
