969d32793d5673c24e14e21b2aee18094a9dacff
misturlee <vugerstar@gmail.com>
2022-03-17 15:15:29 -0700

    try

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - to check the code.
+ *
+ * @c: the character to be checked
+ * Return: 0 means success.
+ */
+int int _isupper(int c);
+{
+	char c;
+
+	if (c <= 90)
+	{
+		return (1);
+	}
+	else if (c > 90)
+	{
+		return (0);
+	}
+	return (0);
+}
