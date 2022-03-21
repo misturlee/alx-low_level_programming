d2a73d068e841df3127b3f832243cf63c5d7a20c
misturlee <vugerstar@gmail.com>
2022-03-21 13:40:44 -0700

    retry

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,11 +1,29 @@
 #include "main.h"
-
 /**
- * reset_to_98 - updates the value of the variable
- * the pointer points to to 98
- * @s: pointer to the variable to update
+ * _atoi - int
+ * @s: pointer
+ * Return: int.
  */
 int _atoi(char *s)
 {
-	*s = 98;
+	int i;
+	int res = 0;
+	int sig = -1;
+	int brk = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == '-')
+			sig = sig * -1;
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			res = res * 10;
+			res -= (s[i] - '0');
+			brk = 1;
+		}
+		else if (brk == 1)
+			break;
+	}
+	res = sig * res;
+	return (res);
 }
